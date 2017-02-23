77ec4af0dc9edc3003fb1ab5ea72d18cad065d75
flouis1 <flouis666@gmail.com>
2017-02-23 18:58:27 +0100

    add script to copy
    Signed-off-by: flouis1 <flouis666@gmail.com>

---
+++ cfg_copy.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+
+#first version
+
+cd ~/
+ln -s ~/conffile/.tmux.conf .tmux.conf
+ln -s ~/conffile/.viminfo .viminfo
+ln -s ~/conffile/.vimrc .vimrc
+ln -sr ~/conffile/.vim .vim
+cd -
